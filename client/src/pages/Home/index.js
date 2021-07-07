c2b3d7c0441ad07916a3ad67a439baa821eb260f
Adam Natrop <adamnatrop@gmail.com>
2021-07-07 15:02:50 -0500

    built route and renderReady state

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import "./style.css"
+
+
+
+
+function Home () {
+
+
+    return(
+        <>
+          
+        </>
+    )
+}
+
+export default Home;
